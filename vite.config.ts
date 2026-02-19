4e00d461845dd82d0f3da5d6ddbb8d62eaf69100
Rukkaman <basquecoastriders@gmail.com>
2026-02-19 19:54:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/porramotogp2025/", // <--- AÑADE ESTA LÍNEA (con los slashes /)
   server: {
     host: "::",
     port: 8080,
